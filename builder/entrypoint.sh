11d62ed4cd3a3a0ea1d635201e79f8a069d4efbf
Chen Fang <mtdcy.chen@gmail.com>
2025-03-10 04:36:30 +0000

    builder: allow run as root

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -5,20 +5,19 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-# enter container root
-[ "$*" = "root" ] && exec bash -li
+# start a login shell
+[ -n "$*" ] || exec bash -li
 
 export PUID="${PUID:-0}"
 export PGID="${PGID:-0}"
 
-id -u buildbot &>/dev/null || useradd -U -m -s /bin/bash buildbot
+if [ "$PUID" -gt 0 ]; then
+    id -u buildbot &>/dev/null || useradd -U -m -s /bin/bash buildbot
 
-[ "$PUID" -eq 0 ] || usermod  buildbot -u "$PUID" || true
-[ "$PGID" -eq 0 ] || groupmod buildbot -g "$PGID" || true
-
-if [ -n "$*" ]; then
+    [ "$PUID" -eq 0 ] || usermod  buildbot -u "$PUID" || true
+    [ "$PGID" -eq 0 ] || groupmod buildbot -g "$PGID" || true
     # shellcheck disable=SC2145
     su buildbot -c "$@"
 else
-    su buildbot -c "bash -li"
+    "$@"
 fi
