2019cb0042e0c6f1c8f09051b0159c713ce7cf62
Chen Fang <mtdcy.chen@gmail.com>
2025-04-17 17:50:21 +0800

    update builders
    (cherry picked from commit 6ffc576a78c3adc9014ebf6875dcc6dcc86b0563)

--- builder/entrypoint-msys2.sh
+++ builder/entrypoint-msys2.sh
@@ -5,46 +5,39 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-export WINEDEBUG="${WINEDEBUG:--all}"
-export WINEPREFIX="${WINEPREFIX:-/wine}"
-
-export OSTYPE=msys
-export MSYSTEM="${MSYSTEM:-UCRT64}"
-
-# start a login shell
-[ -n "$*" ] || exec bash -li
-
-# bind workdir to MSYS2
-#  'mount --bind' needs '--cap-add=SYS_ADMIN'
-WORKDIR="$(pwd -P)"
-mkdir -p "/msys64$WORKDIR"
-#ln -sf "$WORKDIR" "/msys64$WORKDIR"
-mount --bind "$WORKDIR" "/msys64$WORKDIR"
-
 export PUID="${PUID:-0}"
 export PGID="${PGID:-0}"
 
+export WINEPREFIX="${WINEPREFIX:-/wine}"
+export WINEDEBUG="${WINEDEBUG:--all}"
+export WINEPATH="${WINEPATH:-C:\\msys64\\usr\\bin}"
+
+# wine+msys2 reports OSTYPE=cygwin
+export OSTYPE=msys
+export MSYSTEM="${MSYSTEM:-UCRT64}"
+
+WORKDIR="$(pwd -P)"
+#mkdir -p "$(dirname "/msys64$WORKDIR")"
+#ln -sf "$WORKDIR" "/msys64$WORKDIR"
+# symlink: realpath will flat to a different path
+#   => mount bind => needs '--cap-add=SYS_ADMIN'
+mkdir -p "/msys64$WORKDIR"
+mount --bind "$WORKDIR" "/msys64$WORKDIR"
+
 if [ "$PUID" -gt 0 ]; then
-    id -u buildbot &>/dev/null || useradd -U -m -s /bin/bash buildbot
-
-    [ "$PUID" -eq 0 ] || usermod  buildbot -u "$PUID" || true
-    [ "$PGID" -eq 0 ] || groupmod buildbot -g "$PGID" || true
-
-    export WINEPREFIX=${WINEPREFIX:-~buildbot/.wine}
-
-    if [ ! -d "$WINEPREFIX" ]; then
-        su buildbot -c "wine winecfg /v ${WINDOWS:-win10}"
-        ln -sfv /msys64 "$WINEPREFIX/dosdevices/d:"
-        chown -R buildbot:buildbot /msys64
-    fi
-
-    # ln: failed to create symbolic link '/etc/mtab': Permission denied
-    chown buildbot:buildbot "/msys64/etc/mtab"
+    [ "$PUID" -eq "$(id -u buildbot)" ] || usermod  buildbot -u "$PUID" || true
+    [ "$PGID" -eq "$(id -u buildbot)" ] || groupmod buildbot -g "$PGID" || true
 
     # wine only test ownership on top directory
     chown buildbot:buildbot "$WINEPREFIX"
 
-    su buildbot -c "xvfb-run -a wine bash.exe -li -c '$@'"
+    # ln: failed to create symbolic link '/etc/mtab': Permission denied
+    chown buildbot:buildbot "/msys64/etc/mtab"
+
+    [ -n "$1" ] && su buildbot -c "msys2 -l -c '$@'" || su buildbot -c msys2
 else
-    xvfb-run -a wine bash.exe -li -c "$@"
+    # wine only test ownership on top directory
+    chown root:root "$WINEPREFIX"
+
+    [ -n "$1" ] && msys2 -l -c "$@" || msys2
 fi
--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -1,21 +1,17 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ "$(id -u)" -ne 0 ]; then
     echo "Please run as root and set PUID|PGID instead."
     exit 1
 fi
 
-# start a login shell
-[ -n "$*" ] || exec bash -li
-
 export PUID="${PUID:-0}"
 export PGID="${PGID:-0}"
 
 if [ "$PUID" -gt 0 ]; then
-    id -u buildbot &>/dev/null || useradd -U -m -s /bin/bash buildbot
+    [ "$PUID" -eq "$(id -u buildbot)" ] || usermod  buildbot -u "$PUID" || true
+    [ "$PGID" -eq "$(id -u buildbot)" ] || groupmod buildbot -g "$PGID" || true
 
-    [ "$PUID" -eq 0 ] || usermod  buildbot -u "$PUID" || true
-    [ "$PGID" -eq 0 ] || groupmod buildbot -g "$PGID" || true
     # shellcheck disable=SC2145
     su buildbot -c "$@"
 else
