e3d3cb493b9c8f57ecd598368790581cf49b8f1f
Chen Fang <mtdcy.chen@gmail.com>
2025-09-04 16:15:51 +0800

    add nvim

---
+++ nvim/README.md
@@ -0,0 +1,12 @@
+# nvim
+
+## Quick Start
+
+```shell
+# Github
+curl -fsSL https://raw.githubusercontent.com/mtdcy/Dockerfiles/main/nvim/nvim.sh | sudo tee /usr/local/bin/nvim
+# CN
+curl -fsSL http://git.mtdcy.top/mtdcy/Dockerfiles/raw/branch/main/nvim/nvim.sh | sudo tee /usr/local/bin/nvim
+
+chmod a+x /usr/local/bin/nvim
+```
---
+++ nvim/nvim.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+IMAGE=nvim:latest
+
+if [ -z "$(docker image ls "$IMAGE" -q)" ]; then
+    if curl --fail -sIL http://lcr.io -o /dev/null; then
+        IMAGE=lcr.io/mtdcy/nvim:latest
+    else
+        IMAGE=ghcr.io/mtdcy/nvim:latest
+    fi
+    docker pull "$IMAGE"
+    docker tag "$IMAGE" nvim:latest
+fi
+
+opts+=( --user "$(id -u):$(id -g)" -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group )
+
+opts+=( -v "$HOME:$HOME" )
+
+opts+=( -w "$PWD" )
+
+[[ "$PWD" =~ ^$HOME ]] || opts+=( -v "$PWD:$PWD" )
+
+for x in "$@"; do
+    case "$x" in
+        -*) ;;
+        *)
+            test -f "$x" || touch "$x"
+            x="$(realpath "$x")"
+            [[ "$x" =~ "$HOME" ]] || [[ "$x" =~ "$PWD" ]] || opts+=( -v "$x:$x" )
+            ;;
+    esac
+done
+
+docker run -it --rm "${opts[@]}" "$IMAGE" nvim "$@"
