46a9ce552b8d71b54e294effd1ffae12330e3185
Chen Fang <mtdcy.chen@gmail.com>
2025-09-05 16:59:42 +0800

    nginx: rename nginx/ui.ini

--- nginx/rootfs/entrypoint.d/nginx-ui.sh
+++ nginx/rootfs/entrypoint.d/nginx-ui.sh
@@ -7,6 +7,8 @@ NGX_UI_LOGFILE="/var/log/nginx/ui.log"
 
 mkdir -p "$(dirname "$NGX_UI_LOGFILE")"
 
-test -f /etc/nginx/nginx-ui.ini || cp -fv /etc/nginx.default/nginx-ui.ini /etc/nginx/
+# nginx-ui配置好像不太稳定，更新之后原配置会导致一定问题
+#  => 如遇到问题，请删除原有配置文件，重新安装
+test -f /etc/nginx/ui.ini || touch /etc/nginx/ui.ini
 
-exec nginx-ui -config /etc/nginx/nginx-ui.ini > "$NGX_UI_LOGFILE" 2>&1
+exec nginx-ui -config /etc/nginx/ui.ini > "$NGX_UI_LOGFILE" 2>&1
