ded45ff62b5c2b5c9307186e27e99b772a71d768
Chen Fang <mtdcy.chen@gmail.com>
2025-09-05 21:05:32 +0800

    nvim: add option --update

--- nvim/nvim.sh
+++ nvim/nvim.sh
@@ -2,7 +2,7 @@
 
 IMAGE=nvim:latest
 
-if [ -z "$(docker image ls "$IMAGE" -q)" ]; then
+if [ "$*" = "--update" ] || [ -z "$(docker image ls "$IMAGE" -q)" ]; then
     if curl --fail -sIL http://lcr.io -o /dev/null; then
         IMAGE=lcr.io/mtdcy/nvim:latest
     else
@@ -11,6 +11,8 @@ if [ -z "$(docker image ls "$IMAGE" -q)" ]; then
     docker pull "$IMAGE"
     docker tag "$IMAGE" nvim:latest
 fi
+
+[ "$*" = "--update" ] && exit 0
 
 opts+=( --user "$(id -u):$(id -g)" -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group )
 
