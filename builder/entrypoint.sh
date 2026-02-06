52fb061e944917af1152446c7f74736c22ce9ad0
Chen Fang <mtdcy@MacMini-M4.local>
2026-02-06 20:00:25 +0800

    sudo: handle_sigchld_pty: waitpid: No child processes

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -5,18 +5,26 @@ export PGID="${PGID:-0}"
 
 cmd=( "${@:-bash}" )
 
+# sudo: handle_sigchld_pty: waitpid: No child processes
+reap_children() {
+    while wait -n; do
+        :
+    done
+}
+trap 'reap_children' SIGCHLD # trap SIGCHLD => no exec permitted
+
 if [ "$(id -u)" -ne 0 ]; then
-    exec "${cmd[@]}"
+    bash -c "${cmd[*]}"
 else
-    getent passwd "$PUID" || usermod  buildbot -u "$PUID" || true
-    getent group  "$PGID" || groupmod buildbot -g "$PGID" || true
+    getent passwd "$PUID" >/dev/null || usermod  buildbot -u "$PUID" || true
+    getent group  "$PGID" >/dev/null || groupmod buildbot -g "$PGID" || true
 
     # su in alpine has no '--pty' option
     #su buildbot --pty -c "$*"
     # --pty: pseudo-terminal
     # --login: will change the workdir and clear envs
 
-    exec sudo -u "#$PUID" -g "#$PGID" -E -H "${cmd[@]}"
+    sudo -u "#$PUID" -g "#$PGID" -E -H -s /bin/bash -c "${cmd[*]}"
     # -E: preserve envs (no login shell)
     # -H: set HOME
 fi
